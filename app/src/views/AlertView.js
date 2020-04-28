802de605bd05c7392dd71e3fc5f3f4c9225b653f
James Kim <jameskim.1997@yahoo.com>
2020-04-28 14:27:35 -0700

    footerview and alertview

---
+++ app/src/views/AlertView.js
@@ -0,0 +1,13 @@
+// Arguments - Error message
+// Pre conditions - Request to web server fails
+// Post conditions - Display error in modal
+// Returns a modal
+class AlertView {
+  render() {
+    return (
+      <div>
+        {/* Alert component */}
+      </div>
+    )
+  }
+}
---
+++ app/src/views/FooterView.js
@@ -0,0 +1,13 @@
+// Arguments - N/A
+// Pre conditions - N/A
+// Post conditions - N/A
+// Returns a footer component with contact information.
+class FooterView {
+  render() {
+    return (
+      <div>
+        {/* Footer component */}
+      </div>
+    )
+  }
+}
