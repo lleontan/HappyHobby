0d4e643ba3315cbdf98a850ba6142c5c9b113908
James Kim <jameskim.1997@yahoo.com>
2020-04-28 15:50:01 -0700

    dailyhobbyview, interestselectionview, and loginview

---
+++ app/src/client/views/DailyHobbyView.js
@@ -0,0 +1,13 @@
+// Arguments - HobbyModel
+// Pre conditions - No error in recieving model
+// Post conditions - Displays recieved hobby
+// Returns a login component
+class DailyHobbyView {
+  render() {
+    return (
+      <div>
+        {/* hobby component */}
+      </div>
+    )
+  }
+}
---
+++ app/src/client/views/LoginView.js
@@ -0,0 +1,13 @@
+// Arguments - N/A
+// Pre conditions - N/A
+// Post conditions - User is logged in
+// Returns a login component
+class LoginView {
+  render() {
+    return (
+      <div>
+        {/* Login component */}
+      </div>
+    )
+  }
+}
