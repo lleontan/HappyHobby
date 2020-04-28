49fab5395e4ec87bcd6484b4b007511176923a87
James Kim <jameskim.1997@yahoo.com>
2020-04-28 14:04:48 -0700

    all views, models, and controllers

--- app/src/views/HeaderView.js
+++ app/src/views/HeaderView.js
@@ -1,5 +1,5 @@
-let constants = require('./constants');
-import React, {Component} from 'react';
+let constants = require('../utilities/constants');
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { HashLink as Link } from 'react-router-hash-link';
 
@@ -8,14 +8,14 @@ import './common.css';
 /*
   This file contains the common navigation banner between different pages.
 */
-const BANNER_TITLE="HappyHobby";
-const HOMEPAGE_LINK={
-  text:"Home",
-  link:"/"
+const BANNER_TITLE = "HappyHobby";
+const HOMEPAGE_LINK = {
+  text: "Home",
+  link: "/"
 };
-class HeaderView{
-  render(){
-    return(
+class HeaderView {
+  render() {
+    return (
       <div className='NavBanner'>
         <div className=''>
           <Link to={HOMEPAGE_LINK.link}>{HOMEPAGE_LINK.text}</Link>
